a9c8062cd4eb2dcb3f4ab4ad43db70961e25aea4
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-02-20 06:42:24 +0100

    finished the trivia page

--- lib/trivia_page.dart
+++ lib/trivia_page.dart
@@ -8,6 +8,52 @@ class TriviaPage extends StatefulWidget {
 class _TriviaPageState extends State<TriviaPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                'This is where the questions will go',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: TextButton(
+              child: Text(
+                'True',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green)),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: TextButton(
+              child: Text(
+                'False',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red)),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
